2737ed1915280fd7b737cef313ec149d634f426b
Abdellah <abdallahkaba98@gmail.com>
2024-02-27 19:27:13 +0000

    Recherche

--- src/main/java/com/example/hospitalmvc/repositories/PatientRepository.java
+++ src/main/java/com/example/hospitalmvc/repositories/PatientRepository.java
@@ -1,7 +1,10 @@
 package com.example.hospitalmvc.repositories;
 
 import com.example.hospitalmvc.entites.Patient;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface PatientRepository extends JpaRepository<Patient,Long> {
+    Page<Patient> findByNomContains(String kw, Pageable pageable);
 }
