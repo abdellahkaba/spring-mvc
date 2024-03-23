ad090d688e5d168aba740f564deff12faec15e24
Abdellah <abdallahkaba98@gmail.com>
2024-03-23 21:07:00 +0000

    Security1

--- src/main/java/com/example/hospitalmvc/HospitalMvcApplication.java
+++ src/main/java/com/example/hospitalmvc/HospitalMvcApplication.java
@@ -6,6 +6,8 @@ import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 
 import java.util.Date;
 
@@ -29,5 +31,9 @@ public class HospitalMvcApplication {
                 System.out.println(p.getNom());
             });
         };
+    }
+    @Bean
+    public PasswordEncoder passwordEncoder(){
+       return new BCryptPasswordEncoder();
     }
 }
---
+++ src/main/java/com/example/hospitalmvc/security/SecurityConfig.java
@@ -0,0 +1,35 @@
+package com.example.hospitalmvc.security;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfig {
+    @Bean
+    public InMemoryUserDetailsManager inMemoryUserDetailsManager (PasswordEncoder passwordEncoder){
+        String encodePassword = passwordEncoder.encode("Kaba987k");
+        System.out.println(encodePassword);
+        return new InMemoryUserDetailsManager(
+
+                User.withUsername("user1").password(encodePassword).roles("USER").build(),
+                User.withUsername("admin").password(encodePassword).roles("USER","ADMIN").build()
+        );
+    }
+
+    @Bean
+    public SecurityFilterChain securityFilterChain (HttpSecurity httpSecurity) throws Exception {
+        return httpSecurity
+                .formLogin(Customizer.withDefaults())
+                .authorizeHttpRequests(ar -> ar.requestMatchers("/delete/**").hasRole("ADMIN"))
+                .authorizeHttpRequests(ar -> ar.anyRequest().authenticated())
+                .build();
+    }
+}
